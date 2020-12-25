20e6cc7c81a730a5edaab030ba3d3c99718a621c
Shaifali Agrawal <shaifaliagrawal@Shaifalis-MacBook-Air.local>
2020-12-25 14:56:28 +0530

    costants for api routes of template and image generation service added.

--- ad_studio/constants.py
+++ ad_studio/constants.py
@@ -1,1 +1,3 @@
-TEMPLATE_SERVICE = "template-service/v1/template/"
+AD_STUDIO_SERVICE = "http://www.ad_studio.com/"
+TEMPLATE_SERVICE_WITH_ID = "template-service/v1/template/"
+IMAGE_SERVICE = "image-service/v1/generate/"
