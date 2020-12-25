f2831963886bcbc9b5951fbde27191d95044311c
Shaifali Agrawal <shaifaliagrawal@Shaifalis-MacBook-Air.local>
2020-12-25 15:01:48 +0530

    generate_image implemented

--- ad_studio/images.py
+++ ad_studio/images.py
@@ -1,2 +1,11 @@
+import requests
+
+from .constants import AD_STUDIO_SERVICE, IMAGE_SERVICE
+
+
 def generate_image(template):
-	pass
+	image_service_route = AD_STUDIO_SERVICE+IMAGE_SERVICE
+	try:
+		requests.post(image_service_route)
+	except requests.exceptions.ConnectionError:
+		return
