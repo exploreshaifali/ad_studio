33de6bfacb9aa9ce4a4800dd575d3eac01467cdb
Shaifali Agrawal <shaifaliagrawal@Shaifalis-MacBook-Air.local>
2020-12-29 01:14:49 +0530

    basic test for hello world added

--- tests/test_ad_studio.py
+++ tests/test_ad_studio.py
@@ -1,5 +1,18 @@
+from fastapi.testclient import TestClient
+
 from ad_studio import __version__
+
+from ad_studio.main import app
+
+
+client = TestClient(app)
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_index():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello world!"}
