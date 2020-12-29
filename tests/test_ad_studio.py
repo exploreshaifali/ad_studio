9c69e7fcf0a6e21ce7f358c09db5f30ed321ee24
Shaifali Agrawal <shaifaliagrawal@Shaifalis-MacBook-Air.local>
2020-12-29 16:46:51 +0530

    success test case for post feed route

--- tests/test_ad_studio.py
+++ tests/test_ad_studio.py
@@ -1,7 +1,8 @@
+import os
+
 from fastapi.testclient import TestClient
 
 from ad_studio import __version__
-
 from ad_studio.main import app
 
 
@@ -16,3 +17,10 @@ def test_index():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello world!"}
+
+
+def test_process_feed():
+	csv_data = open(os.path.dirname(__file__)+"/test_data/data_feed.csv", "rb")
+	response = client.post("/feed/?template_id=3", files={"file": csv_data, "content_type": "multipart/form-data"})
+	assert response.status_code == 201
+	assert response.json() == {"message": "Images Generated"}
